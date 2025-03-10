d9359c39eacefb582060bd0ca5fdcee857262e52
Bofi1 <angelborque03github@gmail.com>
2025-03-10 12:21:47 -0700

    finished

--- functions.js
+++ functions.js
@@ -1,31 +1,63 @@
+let arrowLeft = document.getElementById("arrow-left")
+let borderColor = document.getElementsByClassName("step")
+let stepName = document.getElementsByClassName("name-step")
+let imgCheck = document.getElementsByClassName("step-img")
+let bar = document.getElementsByClassName("green-bar")
+let arrowR = document.getElementById("arrow-right")
+let arrowL = document.getElementById("arrow-left")
+let progress = 0;
+let secuence = -1;
 
+
+// -------- ASCENDENTE --------
+
+
+    // --- funciones principales
 function prueba() {
-    if (animacionEnCurso == true) { // Verifica si no hay animación en curso
-        animacionEnCurso = true; // Establece la bandera
-        findBar();
-        progress++;
-    }
-
-
-    if (animacionEnCurso == false) { // Verifica si no hay animación en curso
-        animacionEnCurso = true; // Establece la bandera
-        findBar();
-        progress++;
-    }
+    progress++
+    secuence++
+    findBar();
+    arrowLeftActived()
 }
 
 function animation() {
-    setTimeout(borderGreen,400)
-    setTimeout(crossToCheck,400)
-    setTimeout(showName,400)
+    borderGreen()
+    crossToCheck()
+    showName()
     barAnimation()
-
-    bar[secuence].addEventListener('transitionend', animacionFinalizada); //
 }
 
-function animacionFinalizada() { //
-    bar[secuence].removeEventListener('transitionend', animacionFinalizada);
-    animacionEnCurso = false; // Restablece la bandera cuando la animación termina
+function findBar() {
+    console.log("findBar es igual a " + secuence);
+    
+
+    if (secuence == 0) {
+        animation()
+    }
+    if (secuence == 1) {
+        animation()
+    }
+    if (secuence == 2) {
+        animation()
+    }
+    if (secuence == 3) {
+        animation()
+    }
+}
+    // --- /funciones principales
+
+
+
+
+    // llamar a la funcion ascendente
+arrowR.addEventListener("click", prueba)
+
+
+
+
+    // --- funcones secundarias para la accion ascendente
+function arrowLeftActived() {
+    arrowLeft.classList.add("arrow")
 }
 
 function borderGreen() {
@@ -40,39 +72,83 @@ function showName() {
     document.getElementsByClassName("name-step")[secuence].style.opacity = "1";
 }
 
-let borderColor = document.getElementsByClassName("step")
-let stepName = document.getElementsByClassName("name-step")
-let imgCheck = document.getElementsByClassName("step-img")
-let bar = document.getElementsByClassName("green-bar")
-let progress = 0;
-let secuence = 0;
-let animacionEnCurso = false
-
 function barAnimation() {
     bar[secuence].style.width = "242.5rem";
 }
+    // --- /funcones secundarias para la accion ascendente
 
-function findBar() {
-    console.log(progress);
-    if (progress == 0) {
-        animation()
-    }
-    if (progress == 1) {
-        secuence++
-        animation()
-    }
-    if (progress == 2) {
-        secuence++
-        animation()
-    }
-    if (progress == 3) {
-        secuence++
-        animation()
-    }
+
+// -------- /ASCENDENTE --------
+
+
+
+
+
+
+// -------- DECRECIENTE --------
+
+
+    // --- funciones principales
+function pruebaReverse() {
+    findBarReverse();  
+    secuence = secuence - 1   
 }
 
+function animationReverse() {
+    borderGreenReverse()
+    crossToCheckReverse()
+    showNameReverse()
+    barAnimationReverse()
+}
+
+function findBarReverse() {
+    console.log("la secuencia para el findBarReverse es " + secuence);
+    
+
+    if (secuence == 3) {       
+        animationReverse()
+    }
+    if (secuence == 2) {
+        animationReverse()
+    }
+    if (secuence == 1) {
+        animationReverse()
+    }
+    if (secuence == 0) {
+        animationReverse()
+    }
+}
+// --- /funciones principales
 
 
-let crossLeft = document.getElementById("arrow-right")
 
-crossLeft.addEventListener("click", prueba)
+
+    // llamar a la funcion decreciente
+arrowL.addEventListener("click", pruebaReverse)
+
+
+
+
+    // --- funcones secundarias para la accion ascendente
+function borderGreenReverse() {
+    borderColor[secuence].style.borderColor = "#D5D5D5";
+}
+
+function crossToCheckReverse() {
+    imgCheck[secuence].src = "./img/x-gray.png";
+}
+
+function showNameReverse() {
+    document.getElementsByClassName("name-step")[secuence].style.opacity = "0";
+}
+
+function barAnimationReverse() {
+    bar[secuence].style.width = "0";
+}
+    // --- /funcones secundarias para la accion ascendente
+
+
+
+
+// -------- /DECRECIENTE --------
+
